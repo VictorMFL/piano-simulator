e642a82e59d20ade038d250eeb547ec31f0b01f6
VictorMFL <vm859222@gmail.com>
2023-10-02 17:41:43 -0300

    feat: add som ao clicar, ou ao clicar na tecla do seu teclado

---
+++ src/scripts/engine.js
@@ -0,0 +1,30 @@
+const pianoKeys = document.querySelectorAll(".piano-keys .key");
+
+let mapedKeys = [];
+let audio = new Audio("src/audios/a.wav");
+
+const playTune = (key) => {
+  audio.src = `src/audios/${key}.wav`;
+  audio.play();
+
+  const clickedKey = document.querySelector(`[data-key="${key}"]`);
+  clickedKey.classList.add("active");
+
+  setTimeout(() => {
+    clickedKey.classList.remove("active");
+  }, 150);
+};
+
+pianoKeys.forEach((key) => {
+  console.log(key.dataset.key);
+  key.addEventListener("click", () => {
+    playTune(key.dataset.key);
+  });
+  mapedKeys.push(key.dataset.key)
+});
+
+document.addEventListener("keydown", (e) => {
+  if(mapedKeys.includes(e.key)) {
+    playTune(e.key);
+  }
+});
